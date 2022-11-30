c005a3e33f170039e52af4ce08e97b1bafee3f79
Fedor Sharaputo <sharaputo@icloud.com>
2022-11-30 14:01:58 +0300

    404 page added

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="not-found">
+    <h1>Dogs are not found!</h1>
+    <p>
+      You can find some
+      <router-link to="/">here</router-link>
+    </p>
+  </section>
+</template>
+
+<style lang="less" scoped>
+  .not-found {
+    text-align: center;
+    padding-top: 100px;
+    h1 {
+      font-size: 2.2rem;
+      text-transform: uppercase;
+    }
+    p {
+      margin-top: 30px;
+    }
+    a {
+      font-weight: 700;
+      color: @ronchi;
+    }
+  }
+  @media @md {
+    .not-found {
+      h1 {
+        font-size: 2.6rem;
+      }
+      p {
+        font-size: 1.8rem;
+      }
+    }
+  }
+</style>
