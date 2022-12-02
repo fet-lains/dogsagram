17123b92b09948fdcdb1d7ef5556bca8c850e287
Fedor Sharaputo <sharaputo@icloud.com>
2022-12-02 15:43:41 +0300

    added hints for mobile users

--- src/components/layout/TheGrid.vue
+++ src/components/layout/TheGrid.vue
@@ -4,6 +4,7 @@
       <h1 class="dogs__title">
         <slot name="title" />
       </h1>
+      <p class="dogs__hint">Tap twice to toggle favourite!</p>
       <AppLoader v-if="store.isLoading" />
       <p class="dogs__error" v-else-if="!store.isLoading && store.error">
         {{ store.error }}
@@ -29,39 +30,54 @@
       font-size: 2.2rem;
       text-align: center;
       text-transform: uppercase;
-      margin-bottom: 20px;
+    }
+    &__hint {
+      text-align: center;
+      margin-top: 20px;
     }
     &__grid {
       display: grid;
       gap: 30px;
+      margin-top: 20px;
     }
     &__error {
       font-size: 2rem;
       font-weight: 700;
       text-align: center;
+      margin-top: 20px;
     }
   }
 
-  @media @md {
+  @media @sm {
     .dogs {
       padding: 40px 0;
       &__title {
         font-size: 2.6rem;
-        margin-bottom: 30px;
+      }
+      &__hint {
+        margin-top: 30px;
       }
       &__grid {
         grid-template-columns: repeat(2, 1fr);
+        margin-top: 30px;
+      }
+      &__error {
+        margin-top: 30px;
       }
     }
   }
   @media @lg {
     .dogs {
       padding: 50px 0;
-      &__title {
-        margin-bottom: 40px;
+      &__hint {
+        display: none;
       }
       &__grid {
         grid-template-columns: repeat(3, 1fr);
+        margin-top: 40px;
+      }
+      &__error {
+        margin-top: 40px;
       }
     }
   }
