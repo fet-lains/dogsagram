2dafe52b4c6fdd9ecf308b549411ca60ccf0b1d1
Fedor Sharaputo <sharaputo@icloud.com>
2022-12-02 12:47:31 +0300

    removed touch lib

--- src/main.js
+++ src/main.js
@@ -2,22 +2,8 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import { createPinia } from 'pinia';
-import useDoubleTap from '@/composables/useDoubleTap.js';
 import '@/style.css';
-
-// Touch events library
-// https://www.npmjs.com/package/vue3-touch-events
-import Vue3TouchEvents from 'vue3-touch-events';
 
 const app = createApp(App);
 
-app
-  .use(router)
-  .use(createPinia())
-  .use(Vue3TouchEvents)
-  .directive('double-tap', {
-    beforeMount: (el) => {
-      useDoubleTap(el);
-    },
-  })
-  .mount('#app');
+app.use(router).use(createPinia()).mount('#app');
