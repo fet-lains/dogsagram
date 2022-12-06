4879e12b83cb357ea663aad1a82a73868f4ea364
Fedor Sharaputo <sharaputo@icloud.com>
2022-12-06 16:09:05 +0300

    typescript support

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import { createPinia } from 'pinia';
-import '@/style.css';
+import '@/assets/css/style.css';
 
 const app = createApp(App);
 
-app.use(router).use(createPinia()).mount('#app');
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
 
-  base: '/',
-
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 
