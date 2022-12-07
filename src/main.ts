d15c48bfc6150970a883524435ad76f282d0ccbb
Fedor Sharaputo <sharaputo@icloud.com>
2022-12-07 16:47:04 +0300

    click outside directive added

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import { createPinia } from 'pinia';
+import vueClickOutsideElement from 'vue-click-outside-element';
 import '@/assets/css/style.css';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vueClickOutsideElement, 'click-outside');
 
 app.mount('#app');
